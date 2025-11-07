b348eb12fa47a29b22c6b7371408f5e0eba2881e
Philippe Brandon <123750805+PhilippeBRANDON@users.noreply.github.com>
2025-11-07 09:21:08 +0100

    Add new characters to the character pool
    Added new characters Boubou, Ouz Route, Fan dy Rassoul, and Charo line to the character list.

--- characters_pool.py
+++ characters_pool.py
@@ -24,8 +24,13 @@ character_list = [
     Character(name="Champ Billard", max_hp=150, atk=20, defense=40, speed=60, atk_spe=0, def_spe=0, attacks=[slash, punch, kick], sprite="assets/sprites/chanvillard.png"),
     Character(name="Adri Anguille", max_hp=100, atk=100, defense=10, speed=100, atk_spe=0, def_spe=0, attacks=[slash, punch, kick], sprite="assets/sprites/guille.png"),
     Character(name="Nolan", max_hp=70, atk=70, defense=70, speed=70, atk_spe=0, def_spe=0, attacks=[slash, punch, kick], sprite="assets/sprites/nolan.png"),
-    Character(name="Agouno", max_hp=100, atk=100, defense=20, speed=60, atk_spe=0, def_spe=0, attacks=[slash, punch, kick], sprite="assets/sprites/agoun.png")
+    Character(name="Agouno", max_hp=100, atk=100, defense=20, speed=60, atk_spe=0, def_spe=0, attacks=[slash, punch, kick], sprite="assets/sprites/agoun.png"),
+    Character(name="Boubou", max_hp=150, atk=50, defense=50, speed=50, atk_spe=0, def_spe=0, attacks=[slash, punch, kick], sprite="assets/sprites/boubou.png"),
+    Character(name="Ouz Route", max_hp=100, atk=100, defense=100, speed=10, atk_spe=0, def_spe=0, attacks=[slash, punch, kick], sprite="assets/sprites/ouzrout.png"),
+    Character(name="Fan dy Rassoul", max_hp=10, atk=10, defense=10, speed=50, atk_spe=0, def_spe=0, attacks=[slash, punch, kick], sprite="assets/sprites/fan.png"),
+    Character(name="Charo line", max_hp=90, atk=80, defense=60, speed=60, atk_spe=0, def_spe=0, attacks=[slash, punch, kick], sprite="assets/sprites/charoline.png")
 ]
+
 
 
 
